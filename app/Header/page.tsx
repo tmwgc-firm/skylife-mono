dcd8ff5f4b3803d95ca82bb0de63c6a56c80095e
shriharini10 <shri13anitha@gmail.com>
2024-06-22 18:12:57 +0530

    fix: cursor pointer and link is added

--- app/Header/page.tsx
+++ app/Header/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./header.module.css";
+import Link from "next/link";
 
 export default function Headerpage() {
     return (
         <div className={styles.header}>
-        <Image src='/Logo.svg' alt="Logo image" height={40} width={190} />
+        <Link href='./'><Image src='/Logo.svg' alt="Logo image" height={40} width={190} /></Link>
 
         <button>Get in touch</button>
       </div>
